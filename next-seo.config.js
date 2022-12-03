e68facce495b7e1ebf957ad2a16aea606ca52dd1
Robbie Crenshaw <robbiecdesigns@gmail.com>
2022-12-02 20:57:28 -0500

    update next-seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,6 +1,6 @@
 const description = `Learn how to build modern marketing websites, with localization and SEO, using GraphCMS, NextJS, Chakra UI, and Vercel.`
 const title = `Build Modern Marketing Websites with a Headless CMS`
-const url = `https://marketing-websites.withheadlesscms.com`
+const url = `https://surtide.robbiecrenshaw.dev/`
 
 const seo = {
   title,
@@ -13,8 +13,8 @@ const seo = {
     url
   },
   twitter: {
-    handle: '@GraphCMS',
-    site: '@GraphCMS'
+    handle: '@robbiecren',
+    site: '@robbiecren'
   }
 }
 
